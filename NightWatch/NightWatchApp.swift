66a0719d5c41e6cee2093432afa987a43135aebc
Evan Masterson <evan.masterson@teckro.com>
2022-03-24 15:14:10 +0000

    Initial Commit

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Evan Masterson on 24/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
